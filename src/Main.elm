2b4e71df7f3847b0f1fcab4d9548f40dca856a52
bobross <androgenagonist@gmail.com>
2021-07-14 00:12:07 -0700

    experience, styling, added tech
    ready to start adding dynamic content

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,88 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+azure =
+    { org = "Microsoft Azure AI (via AmplifyCP)"
+    , role = "Lead UX/UI Software Engineer"
+    , date = "May 2021 - July 2021"
+    , description = "Leading developers, designers and data engineers in Europe, India, South America, NZ, and US across several projects to showcase demos of Microsoft Azure AI technology to solve real-world problems for different sects of industry."
+    , accomplishments =
+        [ "Refactored five React apps into a single TOML configurable app"
+        , "Removed redundant or broken code with reusable abstractions"
+        , "Managed deployment and production environments for all apps"
+        , "Set scope and priorities for multiple international teams"
+        , "Partnered with business and technical teammates on requirements, design, and technical delivery of solutions"
+        ]
+    , tech = [ "React", "JavaScript", "Redux", "MSSql Server" ]
+    }
+
+
+pot =
+    { org = "Port of Tacoma (via AmplifyCP)"
+    , role = "Lead Full-stack Software Engineer"
+    , date = "March 2021 - Present"
+    , description = "Overhauling and updating various services for the Port of Tacoma, including new solutions that integrate with legacy APIs."
+    , accomplishments =
+        [ "Building and maintaining Express servers to replace outdated legacy services in their data pipeline"
+        , "Designed Socket.io architecture for real-time data requirements"
+        , "Created performance dashboards for new APIs with React"
+        , "Partnered with business and technical teammates on requirements, design, and technical delivery of solutions"
+        ]
+    , tech = [ "React", "JavaScript", "Socket.io", "MSSql Server" ]
+    }
+
+
+colorado =
+    { org = "Colorado Community College System (via AmplifyCP)"
+    , role = "Full-stack Software Engineer"
+    , date = "August 2019 - Present"
+    , description = "Ongoing project that replaced five 15+ year-old web applications with a single, cloud-based, solution."
+    , accomplishments =
+        [ "Built reusable React components in Typescript"
+        , "Built tests with React Testing-Library and Enzyme"
+        , "Developed standards for React, Redux and SASS architecture"
+        , "Built controllers and endpoints with C# and ASP.net Core"
+        , "Wrote SQL Queries for reporting and data collection features"
+        , "Created open-source solutions for technical requirements"
+        , "Provided ongoing support and feature work"
+        , "Partnered with business and technical teammates on requirements, design, and technical delivery of solutions"
+        ]
+    , tech = [ "React", "TypeScript", "Redux", "SASS", "C#", "MSSql Server", "ASP.Net Core" ]
+    }
+
+
+roles =
+    [ azure
+    , pot
+    , colorado
+    ]
+
+
+role data =
+    div [ class "experience" ]
+        [ p [ class "experience__role" ] [ text data.role ]
+        , p [ class "experience__org" ] [ text data.org ]
+        , p [ class "experience__date" ] [ text data.date ]
+        , p [ class "experience__description" ] [ text data.description ]
+        , ul [ class "experience__accomplishment-list" ]
+            (List.map
+                (\a ->
+                    li [ class "experience__accomplishment-item" ]
+                        [ span [ class "experience__accomplishment-text" ] [ text a ] ]
+                )
+                data.accomplishments
+            )
+        ]
+
+
+experience =
+    div [ class "box" ]
+        [ p [ class "box__title" ] [ text "Technical Work Experience" ]
+        , section [ class "experience--section" ]
+            (List.map role roles)
+        ]
+
+
 skills =
     [ "Software Architecture"
     , "Functional Programming"
@@ -16,17 +98,84 @@ skills =
     ]
 
 
-tools =
-    [ "React . Angular"
-    , "Node . Express . Flask . Dotnet"
-    , "MySQL . PostgreSQL . MS Server"
-    , "MongoDB . CosmoDB"
-    , "Apollo Server . Apollo Client . urql"
-    , "Mongoose . Entity . Dapper"
-    , "Redis . Redux"
+frontEnd =
+    [ "Angular.io"
+    , "BEM"
+    , "HTML / CSS / JS"
+    , "jQuery"
+    , "React / React Native"
+    , "Redux"
+    , "SASS (scss)"
     , "Socket.io"
-    , "OAuth . JWT"
-    , "Linux . Git . Docker . VIM"
+    ]
+
+
+backEnd =
+    [ "ASP.Net Core"
+    , "Django"
+    , "Express.js"
+    , "Flask"
+    , "Node.js"
+    , "OAuth"
+    , "Socket.io"
+    ]
+
+
+databases =
+    [ "CosmoDB"
+    , "MSSql Server"
+    , "MongoDB"
+    , "MySQL"
+    , "Redis"
+    ]
+
+
+orms =
+    [ "Dapper"
+    , "Entity Framework"
+    , "Mongoose.js"
+    , "SQL Alchemy"
+    , "TypeORM"
+    ]
+
+
+devtools =
+    [ "Azure"
+    , "AWS"
+    , "Bash"
+    , "DevOps"
+    , "Docker"
+    , "Git"
+    , "GitHub"
+    , "Jira"
+    , "Linux"
+    , "VIM"
+    ]
+
+
+tools =
+    [ "ASP.Net Core"
+    , "Angular.io"
+    , "CosmoDB"
+    , "Dapper"
+    , "Docker"
+    , "Entity Framework"
+    , "Express.js"
+    , "Flask"
+    , "Git"
+    , "JWT"
+    , "Linux"
+    , "MSSql Server"
+    , "MongoDB"
+    , "Mongoose"
+    , "MySQL"
+    , "Node"
+    , "OAuth"
+    , "React.js"
+    , "Redis"
+    , "Redux"
+    , "Socket.io"
+    , "VIM"
     ]
 
 
@@ -40,20 +189,20 @@ csharp =
 clisp =
     { label = "Common Lisp"
     , interest = 10
-    , skill = 4
+    , skill = 6
     }
 
 
 elm =
     { label = "Elm"
-    , interest = 8
-    , skill = 5
+    , interest = 10
+    , skill = 6
     }
 
 
 fsharp =
     { label = "F#"
-    , interest = 8
+    , interest = 9
     , skill = 3
     }
 
@@ -158,14 +307,30 @@ languagesBox data =
         [ p [ class "box__title" ] [ text "languages" ]
         , ul []
             (List.map details data)
+        , div [ class "legend" ]
+            [ div [ class "legend__key" ]
+                [ div [ class "legend__color legend__color--skill" ] []
+                , p [ class "legend__text" ] [ text "Skill" ]
+                ]
+            , div [ class "legend__key" ]
+                [ div [ class "legend__color legend__color--interest" ] []
+                , p [ class "legend__text" ] [ text "Interest" ]
+                ]
+            ]
         ]
 
 
 toolsBox data =
     div [ class "box" ]
         [ p [ class "box__title" ] [ text "technical & tools" ]
-        , ul []
-            (List.map listItem data)
+        , div []
+            (List.map (\s -> p [ class "highlight" ] [ text s ]) data)
+        ]
+
+
+footer =
+    div [ class "footer" ]
+        [ p [] [ text "Powered by Elm" ]
         ]
 
 
@@ -179,13 +344,13 @@ main =
                 , languagesBox languages
                 , toolsBox tools
                 ]
-            , div [ class "box" ]
-                [ div [ class "summary" ]
-                    [ p [ class "box__title" ] [ text "professional summary" ]
-                    , p [ class "summary__text" ] [ text "Passionate full-stack software developer with 8 years of programming\nexperience specializing in JavaScript, TypeScript, Node.js, React, REST APIs,\nSQL, noSQL, GraphQL, and sockets. Open-source contributor and author." ]
+            , div [ class "page__right-col" ]
+                [ div [ class "box" ]
+                    [ p [ class "box__title" ] [ text "TL;DR" ]
+                    , p [ class "summary__text" ] [ text "Passionate full-stack software developer with eight years of programming experience. Open-source contributor and author. Believes in life-long learning and that hot-sauce is a food group." ]
                     ]
-                , div [ class "box" ]
-                    [ div [ class "experience" ] [ text "dingo" ] ]
+                , experience
                 ]
             ]
+        , footer
         ]
